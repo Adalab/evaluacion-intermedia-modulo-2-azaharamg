8b9b0e35452a643c6edcacaa8284c64fb7cab1f1
Azahara Martinez <azaharamaga@gmail.com>
2019-11-24 20:24:18 +0100

    Add reset game

--- js/main.js
+++ js/main.js
@@ -1,22 +1,32 @@
 "use strict";
 
+const defaultData = {
+  input: "",
+  clue: "Escribe el número y dale a Prueba",
+  try: 0
+};
+
+//Get DOM elements
+const getElements = selector => {
+  return document.querySelector(selector);
+};
+
 //Function to get a random number
 const getRandomNumber = max => {
   return Math.ceil(Math.random() * max);
 };
-const randomNumber = getRandomNumber(100);
+let randomNumber = getRandomNumber(100);
 console.log(`El número random generado es: ${randomNumber}`);
 
 //Function to paint clue result
 const paintClueResult = string => {
-  let element = document.querySelector(".js-clue");
+  let element = getElements(".js-clue");
   element.innerHTML = string;
 };
 
 //Function to compare two numbers
 const checkNumbers = number => {
-  const inputValue = parseInt(document.querySelector(".js-input").value);
-
+  const inputValue = parseInt(getElements(".js-input").value);
   if (inputValue < 1 || inputValue > 100) {
     paintClueResult("El número debe estar entre 1 y 100");
   } else if (inputValue > number) {
@@ -32,14 +42,33 @@ const checkNumbers = number => {
 let acc = 0;
 const counter = () => {
   acc += 1;
-  document.querySelector(".js-try").innerHTML = acc;
+  getElements(".js-try").innerHTML = acc;
 };
 
 //Callback function
 const handleValue = event => {
   event.preventDefault();
-  checkNumbers(randomNumber);
-  counter();
+  //Manage when the user hits the button and the input is empty
+  const inputValue = getElements(".js-input").value;
+  if (inputValue === "") {
+    paintClueResult("Introduce un número correcto");
+  } else {
+    checkNumbers(randomNumber);
+    counter();
+  }
 };
-const buttonEl = document.querySelector(".js-button");
+const buttonEl = getElements(".js-button");
 buttonEl.addEventListener("click", handleValue);
+
+//reset function
+const handleReset = event => {
+  event.preventDefault();
+  getElements(".js-input").value = defaultData.input;
+  getElements(".js-clue").innerHTML = defaultData.clue;
+  getElements(".js-try").innerHTML = defaultData.try;
+  randomNumber = getRandomNumber(100);
+  console.log(`Numero random generado tras reset: ${randomNumber}`);
+};
+
+const resetEl = getElements(".js-reset");
+resetEl.addEventListener("click", handleReset);
