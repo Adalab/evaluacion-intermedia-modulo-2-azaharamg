44afcdc0e25633b8d67299470882ce6fe06268ee
Azahara Martinez <azaharamaga@gmail.com>
2019-11-24 19:24:36 +0100

    Fix js errors

--- js/main.js
+++ js/main.js
@@ -1,43 +1,45 @@
 "use strict";
 
-const randomNumber = function getRandomNumber(max) {
+//Function to get a random number
+const getRandomNumber = max => {
   return Math.ceil(Math.random() * max);
 };
-console.log(randomNumber(100));
+const randomNumber = getRandomNumber(100);
+console.log(`El número random generado es: ${randomNumber}`);
 
-function handleFormSubmit(event) {
-  event.preventDefault();
-}
+//Function to paint clue result
+const paintClueResult = string => {
+  let element = document.querySelector(".js-clue");
+  element.innerHTML = string;
+};
 
+//Function to compare two numbers
 const checkNumbers = number => {
-  const inputValue = document.querySelector(".js-input").value;
-  console.log(inputValue);
-  let pistaEl = document.querySelector(".js-clue");
-  if (inputValue > number) {
-    pistaEl.innerHTML = "Demasiado alto";
+  const inputValue = parseInt(document.querySelector(".js-input").value);
+
+  if (inputValue < 1 || inputValue > 100) {
+    paintClueResult("El número debe estar entre 1 y 100");
+  } else if (inputValue > number) {
+    paintClueResult("Demasiado alto");
   } else if (inputValue < number) {
-    pistaEl.innerHTML = "Demasiado bajo";
-  } else if (inputValue === number) {
-    pistaEl.innerHTML = "Has ganado campeona!!!";
+    paintClueResult("Demasiado bajo");
   } else {
-    pistaEl.innerHTML = "El número debe estar entre 1 y 100";
+    paintClueResult("Has ganado campeona!!!");
   }
 };
 
-const handleValue = () => {
-  handleFormSubmit(event);
-  const number = randomNumber(100);
-  checkNumbers(number);
-  counter();
-};
-
+//Function to count how many times the user tries to win
+let acc = 0;
 const counter = () => {
-  let acc = 0;
-  if (handleValue) {
-    acc += 1;
-  }
+  acc += 1;
   document.querySelector(".js-try").innerHTML = acc;
 };
 
+//Callback function
+const handleValue = event => {
+  event.preventDefault();
+  checkNumbers(randomNumber);
+  counter();
+};
 const buttonEl = document.querySelector(".js-button");
 buttonEl.addEventListener("click", handleValue);
